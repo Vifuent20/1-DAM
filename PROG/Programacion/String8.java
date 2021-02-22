15b61e70a4305efe8004339d4047ae6f7832652c
Vicent Casasús <56671024+Vifuent20@users.noreply.github.com>
2021-02-22 11:22:40 +0100

    Add files via upload

---
+++ PROG/Programacion/String8.java
@@ -0,0 +1,13 @@
+package StringOcho;
+
+public class String8 {
+
+	public static void main(String[] args) {
+		
+		String cadena = "En una noche bien fria un guacamayo cantaba a la deriva";
+		String[] palabras = cadena.split(" ");
+		System.out.println("El texto tiene " + palabras.length + " palabras"); 
+		
+	}
+
+}
